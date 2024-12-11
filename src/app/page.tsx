9f0b7cad523af1dddf0cca6ee5e43cd05a397195
nejcfurh <nejc.furh7@gmail.com>
2024-12-11 09:21:22 +0100

    feat(web): Added a feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly. Yuhuuu!</li>
+          <li>Save and see your changes instantly. Yuhuuu! :D</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
