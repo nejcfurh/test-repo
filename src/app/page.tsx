8f3df674acc5638a461ed2d6bc6bbf9de6ba648d
nejcfurh <nejc.furh7@gmail.com>
2024-12-12 10:09:41 +0100

    feat(web): lets see

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,9 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly. Works now?</li>
+          <li>
+            Save and see your changes instantly. Works now? Looks like it works?
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
