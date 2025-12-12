f2b52da2d15acf1a53768f6732f0e235d1bd8697
nejcfurh <nejc.furh7@gmail.com>
2025-12-12 13:09:49 +0100

    feat(test-repo2): Added a feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children} TEchasdsadadsadasdasdasadsdadsaddadasasdasdasdsadasdadad
+        {children}{' '}
+        TEchasdsadadsadasdasdaadasdadasdasadsdadsaddadasasdasdasdsadasdadad
       </body>
     </html>
   );
