bc9238f9f8cd771f85a81e44ed6febc7061f7659
Nejc Furh <101887698+nejcfurh@users.noreply.github.com>
2025-12-12 13:34:45 +0100

    Release v2.7.0 (#170)
    * chore(test-repo): Improve commit messages
    
    * fix(test-repo2): Improve other stuff
    
    * feat(test-repo3): Do it
    
    * fix(test-repo): Minor stuff
    
    * feat(test-repo2): Added a feature
    
    * ci(test-repo): Fix CI (#169)
    
    * improvement(test-repo): improve data (#171)
    
    * feat(test): Feat 2
    
    * improvement(test-repo): improve data
    
    * feat(test): Feat 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children} TEchasdasdadadasasdasda
+        {children}{' '}
+        TEchasdsadadsadasdasdasadsdadsasadasdadddadasasdasdasdsadasdadadsadadadasdsadsadadadas
       </body>
     </html>
   );
